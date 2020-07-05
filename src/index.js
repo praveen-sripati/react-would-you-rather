bff56341e299749d615b07b407c9c3bd2dab9bb4
Praveen <pvnsripati@gmail.com>
2020-07-06 02:57:00 +0530

    feat: Add Component folder to maintain all the components of the app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reducer from './store/reducers';
 import middleware from './store/middleware'
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 const store = createStore(reducer, middleware);
 
