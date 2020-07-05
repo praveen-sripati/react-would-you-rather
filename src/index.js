b0464cfea92cc12cc987ba6b48f43ddcf8a15ebf
Praveen <pvnsripati@gmail.com>
2020-07-06 02:55:17 +0530

    fix: Modify logger's console.group function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import reducer from './store/reducers';
+import middleware from './store/middleware'
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
