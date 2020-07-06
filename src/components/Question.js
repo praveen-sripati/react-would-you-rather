d4f29c709aa62c923102ccae2d0ed32a6fb16e33
Praveen <pvnsripati@gmail.com>
2020-07-07 01:57:11 +0530

    style: Change look of Question Component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { Card, Avatar, Typography, Button, Divider } from 'antd';
+
+const { Title, Text } = Typography;
 
 class Question extends Component {
   render() {
     return (
-      <Card title="Card title" >
-        Card
+      <Card title="Sarah Edo asks">
+        <div className="question-content">
+          <div>
+            <Avatar
+              src="https://api.adorable.io/avatars/100/sarahedo.png"
+              size={100}
+            />
+          </div>
+          <Divider className="question-content-divider" type="vertical" />
+          <div className="question-content-details">
+            <Title level={4}>Would you rather</Title>
+            <Text>Description</Text>
+            <Button className="question-content-details__poll_btn">
+              View Poll
+            </Button>
+          </div>
+        </div>
       </Card>
     );
   }
 }
 
-export default Question
+export default Question;
