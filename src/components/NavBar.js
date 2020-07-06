c109d203b7e17d4bee4a344f19ccfec169c6844d
Praveen <pvnsripati@gmail.com>
2020-07-07 02:11:06 +0530

    feat: Add icons to the nav-links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Typography, Avatar } from 'antd';
-import { TrophyOutlined } from '@ant-design/icons';
+import {
+  TrophyOutlined,
+  PlusOutlined,
+  HomeOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons';
 
 const { Text } = Typography;
 
@@ -12,26 +17,35 @@ class NavBar extends Component {
         <ul className="start-nav-ul">
           <li>
             <NavLink to="/" exact activeClassName="active">
-              Home
+              <HomeOutlined />
+              <span className="nav-title">Home</span>
             </NavLink>
           </li>
           <li>
             <NavLink to="/new" exact activeClassName="active">
-              New Question
+              <PlusOutlined />
+              <span className="nav-title">New Question</span>
             </NavLink>
           </li>
           <li>
             <NavLink to="/leaderboard" exact activeClassName="active">
               <TrophyOutlined />
-              <span style={{ padding: '5px' }}>Leaderboard</span>
+              <span className="nav-title">Leaderboard</span>
             </NavLink>
           </li>
         </ul>
         <ul className="end-nav-ul">
           <li>
-            <Text className="login-user-title" strong>Hello, Sara <Avatar className="user-avatar" src="https://api.adorable.io/avatars/120/sarahedo.png" /></Text>
+            <Text className="login-user-title" strong>
+              Hello, Sara{' '}
+              <Avatar
+                className="user-avatar"
+                src="https://api.adorable.io/avatars/120/sarahedo.png"
+              />
+            </Text>
             <NavLink to="/login" exact activeClassName="active">
-              Logout
+              <LogoutOutlined />
+              <span className="nav-title">Logout</span>
             </NavLink>
           </li>
         </ul>
