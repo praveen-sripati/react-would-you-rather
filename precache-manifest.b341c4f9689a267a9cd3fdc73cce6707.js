2ed1b9dc5ea0a945442e97671204fd12d2d5ca45
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-09 19:17:30 +0000

    Deploy praveen-sripati/react-would-you-rather to github.com/praveen-sripati/react-would-you-rather.git:gh-pages

---
+++ precache-manifest.b341c4f9689a267a9cd3fdc73cce6707.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4704f3df35774a4711d70900083ec4",
+    "url": "/react-would-you-rather/index.html"
+  },
+  {
+    "revision": "7da1c62d44726fe219b0",
+    "url": "/react-would-you-rather/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "a4a453ca4cc7c60b3319",
+    "url": "/react-would-you-rather/static/css/main.bba94b9c.chunk.css"
+  },
+  {
+    "revision": "7da1c62d44726fe219b0",
+    "url": "/react-would-you-rather/static/js/2.7b4a7e29.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-would-you-rather/static/js/2.7b4a7e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a453ca4cc7c60b3319",
+    "url": "/react-would-you-rather/static/js/main.f0dcc5db.chunk.js"
+  },
+  {
+    "revision": "2b2f5d878ae6e9f9b045",
+    "url": "/react-would-you-rather/static/js/runtime-main.36ba5b7c.js"
+  },
+  {
+    "revision": "1ee972e196155150090bf5a6434ddbce",
+    "url": "/react-would-you-rather/static/media/would-you-rather.1ee972e1.png"
+  }
+]);
